6a968e4ab3c7c04cdf01bf6f073df151ecca3ff9
Abhay Patidar <patidarabhayn1@gmail.com>
2021-05-28 20:19:44 +0530

    Tag End Points

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def sample_user(email='abhay@gmail.com', password='password123'):
+    return get_user_model().objects.create_user(email,password)
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
@@ -29,3 +34,11 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
